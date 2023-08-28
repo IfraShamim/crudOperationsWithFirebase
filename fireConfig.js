20db65ec8ac7e8af8d79613e0605866fed19b18c
Ifra Shamim <128763023+IfraShamim@users.noreply.github.com>
2023-08-28 20:41:19 +0500

    Done!

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBoixarrFXzWRvb7DqSrNYL9sVjtx0tg8g",
+    authDomain: "crudoperations-fa7db.firebaseapp.com",
+    databaseURL: "https://crudoperations-fa7db-default-rtdb.firebaseio.com",
+    projectId: "crudoperations-fa7db",
+    storageBucket: "crudoperations-fa7db.appspot.com",
+    messagingSenderId: "135045442560",
+    appId: "1:135045442560:web:f16aebf43b957c9da73ce5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
